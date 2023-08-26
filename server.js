87764e057e11135967add4b70bae2e52cbd8620c
Ramakrishna G Kannari <92996109+ramkrishnareddy24@users.noreply.github.com>
2023-08-26 15:19:56 +0530

    final code

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js"
 import cors from "cors";
 import path from "path";
+import {fileURLToPath} from 'url;
 
 //configure env
 dotenv.config();
 
 //rest object
 const app = express();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //datebase config
 connectDB();
